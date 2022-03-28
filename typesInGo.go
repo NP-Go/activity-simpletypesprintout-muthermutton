1bca668bcc448f81769293bed2d96e94130f0af1
muthermutton <khairal.fazil@gmail.com>
2022-03-28 16:44:50 +0800

    completed assignment

--- typesInGo.go
+++ typesInGo.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 //Insert variables declarations here based on activity
 
 func tellMeTypes() {
 	//insert code here to print out types of variables
+	text := "The following is the account information"
+	firstName := "Luke"
+	age := 20
+	credit := 123.55
+	subscribe := true
+	fmt.Printf("%T %T %T %T %T", text, firstName, age, credit, subscribe)
 }
 
 func main() {
