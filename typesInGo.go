fad8063223e8b9206635a3d92abc3f7cca3104ac
khaifazil <khairal.fazil@gmail.com>
2022-03-31 15:04:47 +0800

    added more variables to complete assignment

--- typesInGo.go
+++ typesInGo.go
@@ -8,10 +8,15 @@ func tellMeTypes() {
 	//insert code here to print out types of variables
 	text := "The following is the account information"
 	firstName := "Luke"
+	lastName := "Skywalker"
 	age := 20
+	weight := 73.0
+	height := 1.72
 	credit := 123.55
+	accName := "admin"
+	accPassword := "password"
 	subscribe := true
-	fmt.Printf("%T %T %T %T %T", text, firstName, age, credit, subscribe)
+	fmt.Printf("%T %T %T %T %T %T %T %T %T %T", text, firstName, lastName, age, weight, height, credit, accName, accPassword, subscribe)
 }
 
 func main() {
